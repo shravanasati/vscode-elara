b7b3dbfce99aa8cf9ea91b999eae43b5960925b4
Shravan Asati <dev.shravan@protonmail.com>
2025-05-23 19:41:57 +0530

    fix code font and add demo

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,19 @@ import * as vscode from 'vscode';
 
 import { exec } from "child_process";
 
+function checkElaraInstalled(): Promise<boolean> {
+	return new Promise((resolve) => {
+		exec('elara --help', (error, stdout, stderr) => {
+			console.log(`check installed: ${error}, ${stdout}, ${stderr}`);
+			if (error || stderr) {
+				resolve(false);
+			} else {
+				resolve(true);
+			}
+		});
+	});
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -20,7 +33,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('elara.convertNotebookToHTML', () => {
+	const disposable = vscode.commands.registerCommand('elara.convertNotebookToHTML', async () => {
 		// The code you place here will be executed every time your command is executed
 		const editor = vscode.window.activeTextEditor;
 		if (!editor) {
@@ -32,36 +45,42 @@ export function activate(context: vscode.ExtensionContext) {
 			vscode.window.showErrorMessage('The active file is not a Jupyter Notebook (.ipynb).');
 			return;
 		}
-		// Display a message box to the user
-		vscode.window.withProgress({
-			location: vscode.ProgressLocation.Notification,
-			title: "Converting Jupyter Notebook to HTML...",
-			cancellable: false
-		}, async (progress) => {
-			return new Promise<void>((resolve, reject) => {
-				const themeArg = elaraTheme ? `--theme ${elaraTheme}` : '';
-				const fontArg = elaraFont ? `--font ${elaraFont}` : '';
-				const codeFontArg = elaraCodeFont ? `--code-font ${elaraCodeFont}` : '';
-				exec(`elara convert --silent ${filePath} ${themeArg} ${fontArg} ${codeFontArg}`, (error, stdout, stderr) => {
-					if (error) {
-						progress.report({ message: `Conversion failed: ${error.message}` });
-						setTimeout(() => reject(), 2000);
-						return;
-					}
-					if (stderr) {
-						progress.report({ message: `Conversion failed: ${stderr}` });
-						setTimeout(() => reject(), 2000);
-						return;
-					}
-					progress.report({ message: `Conversion successful! Opening file...` });
-					setTimeout(async () => {
-						const htmlFilePath = stdout.trim();
-						const uri = vscode.Uri.file(htmlFilePath);
-						await vscode.env.openExternal(uri);
-						resolve();
-					}, 1000);
-				});
-			});
+
+		const isInstalled = await checkElaraInstalled();
+		if (!isInstalled) {
+			vscode.window.showErrorMessage('Elara is not installed or not found in the PATH.');
+			return;
+		}
+
+		vscode.window.showInformationMessage("Converting Jupyter Notebook to HTML...");
+
+		const themeArg = elaraTheme ? `--theme ${elaraTheme}` : '';
+		const fontArg = elaraFont ? `--font ${elaraFont}` : '';
+		const codeFontArg = elaraCodeFont ? `--code-font ${elaraCodeFont}` : '';
+		const convertCommand = `elara convert --silent ${filePath} ${themeArg} ${fontArg} ${codeFontArg}`;
+		console.log(`Executing command: ${convertCommand}`);
+
+		exec(convertCommand, async (error, stdout, stderr) => {
+			if (error) {
+				vscode.window.showErrorMessage(`Conversion failed: ${error.message}`);
+				return;
+			}
+			if (stderr) {
+				vscode.window.showErrorMessage(`Conversion failed: ${stderr}`);
+				return;
+			}
+
+			const htmlFilePath = stdout.trim();
+			const openButton = 'Open';
+			const result = await vscode.window.showInformationMessage(
+				`Conversion successful! HTML file created at: ${htmlFilePath}`,
+				openButton
+			);
+
+			if (result === openButton) {
+				const uri = vscode.Uri.file(htmlFilePath);
+				await vscode.env.openExternal(uri);
+			}
 		});
 	});
 
